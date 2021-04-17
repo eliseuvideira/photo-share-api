951c682e631ba43b97010d25f6514c3494a5d932
Eliseu Videira <eliseuvideira1@gmail.com>
2021-04-17 15:16:38 -0300

    :sparkles: improving resolvers

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 const { ApolloServer } = require("apollo-server");
 
+const id = (() => {
+  let _id = 0;
+  return () => ++_id;
+})();
+
 const photos = [];
 
 const typeDefs = `
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
+
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
   }
 
   type Mutation {
-    createPhoto(name: String! description: String): Boolean!
+    createPhoto(name: String! description: String): Photo!
   }
 `;
 
 const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
+    allPhotos: () => photos,
   },
 
   Mutation: {
     createPhoto: (parent, args) => {
-      photos.push(args);
-      return true;
+      const photo = { ...args, id: id() };
+      photos.push(photo);
+      return photo;
     },
+  },
+
+  Photo: {
+    url: (parent) => `http://localhost:4000/img/${parent.id}.jpg`,
   },
 };
 
