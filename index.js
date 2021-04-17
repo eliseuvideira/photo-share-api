0a7f4bbc19676cc3730a141671540386cb71f1d0
Eliseu Videira <eliseuvideira1@gmail.com>
2021-04-17 14:52:23 -0300

    :sparkles: added minimal setup

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+const { ApolloServer } = require("apollo-server");
+
+const typeDefs = `
+  type Query {
+    totalPhotos: Int!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    totalPhotos: () => 42,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+(async () => {
+  const { url } = await server.listen();
+
+  console.info(`listening at ${url}`);
+})().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
