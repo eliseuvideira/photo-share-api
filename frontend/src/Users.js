b3d42c382539fb0f7ddce95149726ec4578d9392
Eliseu Videira <eliseuvideira1@gmail.com>
2021-04-18 12:46:26 -0300

    :sparkles: fetching from front-end

---
+++ frontend/src/Users.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Query } from "react-apollo";
+import { ROOT_QUERY } from "./ROOT_QUERY";
+
+const Users = () => (
+  <Query query={ROOT_QUERY}>
+    {({ data, loading, refetch }) =>
+      loading ? (
+        <p>loading users...</p>
+      ) : (
+        <UserList
+          count={data.totalUsers}
+          users={data.allUsers}
+          refetch={refetch}
+        />
+      )
+    }
+  </Query>
+);
+
+const UserList = ({ count, users, refetch }) => (
+  <div>
+    <p>{count} Users</p>
+    <button onClick={() => refetch()}>Refetch</button>
+    <ul>
+      {users.map((user) => (
+        <UserListItem
+          key={user.githubLogin}
+          name={user.name}
+          avatar={user.avatar}
+        />
+      ))}
+    </ul>
+  </div>
+);
+
+const UserListItem = ({ name, avatar }) => (
+  <li>
+    <img src={avatar} width={48} height={48} alt="" />
+    {name}
+  </li>
+);
+
+export default Users;
